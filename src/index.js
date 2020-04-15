d5267bf3cbbb8e1c44fff6d968a80a5155dd9bcd
Hakan Yildiz <hakan@re-coded.com>
2020-04-15 14:53:20 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ window.addEventListener("load", function(e){
    
    for(const li of breeds){
      if(li.textContent.charAt(0) == inputStr){
-        li.style.display = "list-item";
+        li.style.display = "list-item"; //element will be rendered as a list
      }
      
    }
